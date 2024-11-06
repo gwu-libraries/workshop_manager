c99fe069713511620c68fb845fe22b73b234eb12
Alex Boyd <alexanderleeboyd@gmail.com>
2024-11-06 18:35:46 -0500

    Working grouped bar chart on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,26 +1,77 @@
 class DashboardController < ApplicationController
   def show
-    @bar_chart =
-      Vega
-        .lite
-        .data(
-          [
-            { city: 'A', sales: 28 },
-            { city: 'B', sales: 55 },
-            { city: 'C', sales: 43 }
-          ]
-        )
-        .mark(type: 'bar', tooltip: true)
-        .encoding(
-          x: {
-            field: 'city',
-            type: 'nominal'
-          },
-          y: {
-            field: 'sales',
-            type: 'quantitative'
-          }
-        )
+    # all workshops
+
+    workshops = Workshop.all
+
+    result = []
+    workshops.each do |w|
+      result << {
+        category: w.title,
+        group: 'registrations',
+        value: w.workshop_participants.count
+      }
+
+      result << {
+        category: w.title,
+        group: 'attended',
+        value: w.workshop_participants.where(in_attendance: true).count
+      }
+    end
+
+    @bar_chart = {
+      '$schema': 'https://vega.github.io/schema/vega-lite/v5.json',
+      data: {
+        values: result
+      },
+      mark: 'bar',
+      encoding: {
+        x: {
+          field: 'category'
+        },
+        y: {
+          field: 'value',
+          type: 'quantitative'
+        },
+        xOffset: {
+          field: 'group'
+        },
+        color: {
+          field: 'group'
+        }
+      }
+    }
+
+    # require 'pry'
+    # binding.pry
+
+    # {
+    #   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    #   "data": {
+    #     "values": [
+    #       {"category":"A", "group": "x", "value":0.1},
+    #       {"category":"A", "group": "y", "value":0.6},
+    #       {"category":"A", "group": "z", "value":0.9},
+    #       {"category":"B", "group": "x", "value":0.7},
+    #       {"category":"B", "group": "y", "value":0.2},
+    #       {"category":"B", "group": "z", "value":1.1},
+    #       {"category":"C", "group": "x", "value":0.6},
+    #       {"category":"C", "group": "y", "value":0.1},
+    #       {"category":"C", "group": "z", "value":0.2}
+    #     ]
+    #   },
+    #   "mark": "bar",
+    #   "encoding": {
+    #     "x": {"field": "category"},
+    #     "y": {"field": "value", "type": "quantitative"},
+    #     "xOffset": {"field": "group"},
+    #     "color": {"field": "group"}
+    #   }
+    # }
+
+    # by track
+
+    # by facilitator
   end
 
   private
