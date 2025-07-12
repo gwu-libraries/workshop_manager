7baa5aa24cfc381b0c1cc94f20ca020f62121b2f
Alex Boyd <alexanderleeboyd@gmail.com>
2025-07-11 22:11:23 -0400

    Add workshop scope tests

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -11,48 +11,157 @@ RSpec.describe Workshop, type: :model do
     it { should have_many(:facilitators).through(:workshop_facilitators) }
 
     it { should have_many(:participants) }
+
+    it { should have_many :application_questions }
+    it do
+      should have_many(:application_question_responses).through(
+               :application_questions
+             )
+    end
+
+    it { should have_many :feedback_questions }
+    it do
+      should have_many(:feedback_question_responses).through(
+               :feedback_questions
+             )
+    end
   end
 
-  describe 'methods' do
+  describe 'model methods' do
+    it '.registration_required returns all workshops with registration required' do
+      5.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'registration_required'
+        )
+      end
+
+      3.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'no_registration_required'
+        )
+      end
+
+      1.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'application_required'
+        )
+      end
+
+      expect(Workshop.registration_required.count).to eq(5)
+    end
+
+    it '.no_registration_required returns all workshops with no registration required' do
+      5.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'registration_required'
+        )
+      end
+
+      3.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'no_registration_required'
+        )
+      end
+
+      1.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'application_required'
+        )
+      end
+
+      expect(Workshop.no_registration_required.count).to eq(3)
+    end
+
+    it '.application_required returns all workshops with no registration required' do
+      5.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'registration_required'
+        )
+      end
+
+      3.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'no_registration_required'
+        )
+      end
+
+      1.times do
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'application_required'
+        )
+      end
+
+      expect(Workshop.application_required.count).to eq(1)
+    end
+  end
+
+  describe 'instance methods' do
     it '.registration_required returns true if registration is required' do
-      workshop_1 = FactoryBot.create(:future_workshop, 
-                                      registration_modality: 'registration_required')
+      workshop_1 =
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'registration_required'
+        )
 
       expect(workshop_1.registration_required).to eq(true)
     end
 
     it '.registration_required returns false if registration required not selected' do
-      workshop_1 = FactoryBot.create(:future_workshop, 
-                                      registration_modality: 'application_required')
+      workshop_1 =
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'application_required'
+        )
 
       expect(workshop_1.registration_required).to eq(false)
     end
-    
+
     it '.application_required returns true if application is required' do
-      workshop_1 = FactoryBot.create(:future_workshop, 
-                                     registration_modality: 'application_required')
-      
+      workshop_1 =
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'application_required'
+        )
+
       expect(workshop_1.application_required).to eq(true)
     end
 
     it '.application_required returns false if application required not selected' do
-      workshop_1 = FactoryBot.create(:future_workshop, 
-                                      registration_modality: 'registration_required')
+      workshop_1 =
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'registration_required'
+        )
 
       expect(workshop_1.application_required).to eq(false)
     end
-    
+
     it '.no_registration_required returns true if no registration is required' do
-      workshop_1 = FactoryBot.create(:future_workshop, 
-                                      registration_modality: 'no_registration_required')
-      
+      workshop_1 =
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'no_registration_required'
+        )
+
       expect(workshop_1.no_registration_required).to eq(true)
     end
-    
+
     it '.application_required returns false if application required not selected' do
-      workshop_1 = FactoryBot.create(:future_workshop, 
-                                      registration_modality: 'registration_required')
-      
+      workshop_1 =
+        FactoryBot.create(
+          :future_workshop,
+          registration_modality: 'registration_required'
+        )
+
       expect(workshop_1.no_registration_required).to eq(false)
     end
 
